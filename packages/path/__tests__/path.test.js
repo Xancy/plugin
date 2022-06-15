e824b0846ca61962b42a9fff869dd3c714439052
Alex Fang <deeno@qq.com>
2022-06-15 17:22:39 +0800

    chore: update eslint styling and prettifying config

--- packages/path/__tests__/path.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const path = require('..');
-
-describe('path', () => {
-    it('needs tests');
-});
--- packages/path/lib/path.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = path;
-
-function path() {
-    // TODO
-}
